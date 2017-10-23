194743271b61d1097cd2ff77799b4097093ad924
Aleksandr Bratchikov <sashabratchikov@gmail.com>
2017-10-23 11:51:48 +0300

    pep8, some refactoring

--- bars.py
+++ bars.py
@@ -5,41 +5,42 @@ from sys import argv
 
 
 def load_data(filepath):
-    json_file = open(filepath, 'r')
-    json_content = json_file.read()
-    json_file.close()
-    return json.loads(json_content)
+    with open(filepath, 'r') as json_file:
+        return json.loads(json_file.read())
+
+
+def get_distance(point1, point2):
+    return hypot(point2[0] - point1[0], point2[1] - point1[1])
+
+
+def get_bar_name(bar):
+    return bar['properties']['Attributes']['Name']
 
 
 def get_biggest_bar(bars):
-    biggest_bar = max(bars, key = lambda bar: bar['properties']['Attributes']['SeatsCount'])
-    return biggest_bar['properties']['Attributes']['Name']
+    return max(bars, key=lambda bar: bar['properties']
+                                        ['Attributes']
+                                        ['SeatsCount'])
+
 
 def get_smallest_bar(bars):
-    smallest_bar = min(bars, key = lambda bar: bar['properties']['Attributes']['SeatsCount'])
-    return smallest_bar['properties']['Attributes']['Name']
-
-
-# we ignore Earth radius as all points are very close
-def get_distance(coords1, coords2):
-    return hypot(coords2[0] - coords1[0], coords2[1] - coords1[1])
+    return min(bars, key=lambda bar: bar['properties']
+                                        ['Attributes']
+                                        ['SeatsCount'])
 
 
 def get_closest_bar(bars, longitude, latitude):
-    closest_bar_name = bars[0]['properties']['Attributes']['Name']
-    shortest_distance = get_distance(bars[0]['geometry']['coordinates'], [longitude, latitude])
-    for bar in bars[1:]:
-        distance = get_distance(bar['geometry']['coordinates'], [longitude, latitude])
-        if distance < shortest_distance:
-            closest_bar_name = bar['properties']['Attributes']['Name']
-            shortest_distance = distance
-    return closest_bar_name
+    return min(bars, key=lambda bar: get_distance(bar['geometry']
+                                                     ['coordinates'],
+                                                  [longitude, latitude]))
 
 
 if __name__ == '__main__':
     bars = load_data(argv[1])['features']
     latitude = float(input('Please input your latitude: '))
     longitude = float(input('Please input your longitude: '))
-    print('Biggest bar: ', get_biggest_bar(bars))
-    print('Smallest bar: ', get_smallest_bar(bars))
-    print('Closest bar: ', get_closest_bar(bars, longitude, latitude))
+    print('Biggest bar: ', get_bar_name(get_biggest_bar(bars)))
+    print('Smallest bar: ', get_bar_name(get_smallest_bar(bars)))
+    print('Closest bar: ', get_bar_name(get_closest_bar(bars,
+                                                        longitude,
+                                                        latitude)))
